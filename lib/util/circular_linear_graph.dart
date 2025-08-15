2db1678485c165ea037f5034083754710403a2e8
rustyonkeys <kirthishetty56@gmail.com>
2025-08-15 15:22:05 +0530

    added the graph

---
+++ lib/util/circular_linear_graph.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:percent_indicator/circular_percent_indicator.dart';
+
+
+class CircularGraph extends StatelessWidget {
+  final double percent;
+  const CircularGraph({
+    super.key,
+  required this.percent});
+
+  @override
+  Widget build(BuildContext context) {
+    return CircularPercentIndicator(
+      radius: 60,
+      animation: true,
+      lineWidth: 6,
+      percent: percent,
+      backgroundColor: Colors.blueGrey,
+      animateFromLastPercent: true,
+      progressColor: Colors.white,
+      center: Text("${(percent * 100).toStringAsFixed(0)}%",
+      style: TextStyle(
+        fontWeight: FontWeight.w500,
+        color: Colors.white
+      ),),);
+
+  }
+}
+
+
