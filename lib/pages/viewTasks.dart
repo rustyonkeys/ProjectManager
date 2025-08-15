d4a3fd720bba71c037b94e456e8bb97b437e6d08
rustyonkeys <kirthishetty56@gmail.com>
2025-08-15 15:20:24 +0530

    updated the ui

--- lib/pages/viewTasks.dart
+++ lib/pages/viewTasks.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:percent_indicator/circular_percent_indicator.dart';
+import 'package:taskmanager/util/circular_linear_graph.dart';
 
 class ViewTasks extends StatelessWidget {
   final String taskID;
@@ -38,20 +40,56 @@ class ViewTasks extends StatelessWidget {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text(
-                  title,
-                  style: GoogleFonts.bebasNeue(
+                SizedBox(height: 30,),
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(40),
+                  child: Container(
+                    height: 230,
+                    width: double.infinity,
                     color: Colors.black,
-                    fontSize: 32,
-                    fontWeight: FontWeight.bold,
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 16),
+                      child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  CircularGraph(percent: 0.2),
+                                  // CircularPercentIndicator(
+                                  //     radius: 60,
+                                  //   progressColor: Colors.blueGrey,
+                                  //   backgroundColor: Colors.white,
+                                  //   percent: 0.4  ,
+                                  //   lineWidth: 6,
+                                  //   animation: true,
+                                  //   animateFromLastPercent: true,
+                                  //   center: Text('40%',
+                                  //     style: TextStyle(color: Colors.white,
+                                  //         fontWeight: FontWeight.bold),),
+                                  // ),
+                                  SizedBox(width: 10),
+                              //TITLE
+                                      Flexible(
+                                        child: Text(
+                                          title,
+                                          softWrap: true,
+                                          overflow: TextOverflow.visible,
+                                          style: GoogleFonts.bebasNeue(
+                                            color: Colors.white,
+                                            fontSize: 32,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                      ),
+                                ],
+                              ),
+                    ),
                   ),
                 ),
-                const SizedBox(height: 10),
+                const SizedBox(height: 20),
                 Text(
                   description,
                   style: const TextStyle(
-                    color: Colors.black,
-                    fontSize: 18,
+                    color: Colors.grey,
+                    fontSize: 20,
                   ),
                 ),
                 const SizedBox(height: 20),
@@ -116,13 +154,25 @@ class ViewTasks extends StatelessWidget {
                   ),
                 ),
 
-                Center(
-                  child: ElevatedButton(onPressed: () {},
-                      child: Text("Add another subtask",
-                      style: TextStyle(
-                        color: Colors.grey[900]
-                      ),)),
-                )
+
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                       ElevatedButton(onPressed: () {},
+                           style: ElevatedButton.styleFrom(
+                               backgroundColor: Colors.black,
+                               foregroundColor: Colors.white
+                           ),
+                            child: Text("Add another subtask")),
+                      ElevatedButton(onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.red,
+                            foregroundColor: Colors.white
+                          ),
+                          child: Text("Delete task")),
+                    ],
+                  ),
+                SizedBox(height: 20,)
               ],
             ),
           );
